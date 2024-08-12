fd703ce139dce45b2bcaf83d1afd24bea75db5b7
OteIan <oteian9@gmail.com>
2024-08-12 14:22:00 +0300

    lists-and-keys done

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function ProjectItem({ name, about, technologies }) {
+function ProjectItem({ id, name, about, technologies }) {
+  const tech = technologies.map((technology) => 
+    <span key={id}>{technology}</span>
+  )
   return (
-    <div className="project-item">
+    <div className="project-item" key={id}>
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {tech}
       </div>
     </div>
   );
